48e043ef7bde944039c6f66627cbccef6a98e533
JIA KEE <jktan1026@gmail.com>
2022-05-23 21:28:09 +0800

    Initial commit

---
+++ php/register_user.php
@@ -0,0 +1,31 @@
+<?php
+if (!isset($_POST)){
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+include_once("dbconnect.php");
+$email = $_POST['email'];
+$name = $_POST['name'];
+$password = sha1($_POST['password']);
+$na = "na";
+
+$sqlinsert = "INSERT INTO tbl_users(user_email, user_name, user_password, user_phone, 
+user_address) VALUES ('$email','$name','$password','$na','$na')";
+
+if ($conn->query($sqlinsert) === TRUE) {
+    $response = array('status' => 'success', 'data' => null);
+    $filename = mysqli_insert_id($conn);
+    sendJsonResponse($response);
+} else {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+}
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
+?>
